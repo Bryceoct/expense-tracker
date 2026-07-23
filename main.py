b467d20a1a4d46b9c16c1620b55a470d20676ac2
Bryce <bryceharrison97@gmail.com>
2026-07-22 21:11:42 -0400

    Store and display multiple expenses

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from expense import Expense
 
-expense_1 = Expense(1, 125, "bills", "phone bill", "07-27-26")
+expense_1 = Expense(1, 125, "bill", "phone bill", "07-27-26")
+expense_2 = Expense(2, 50, "food", "chipotle", "12-25-2026")
 
-print(expense_1)
+expenses = []
+
+expenses.append(expense_1)
+expenses.append(expense_2)
+
+for expense in expenses:
+    print(expense)
